2f6aca8bc656b9b884ed33b8f52b2097fd1e776c
Stéphanie Bergamo <stephanie@bergamo.fr>
2022-10-09 17:40:07 +0200

    refactor: use common processor

--- pipelines/tasks/departments/department_processor.py
+++ pipelines/tasks/departments/department_processor.py
@@ -1,34 +1,27 @@
 import datetime
 import json
-from io import BytesIO, StringIO
+from io import StringIO
+from typing import Any
 
-import boto3
-import botocore.client
 import geopandas
 import pandas
 from shapely import geometry
 
 from pipelines import DEFAULT_S3_BUCKET
+from pipelines.tasks.departments.processor import Processor
 
 
-class DepartmentProcessor:
+class DepartmentProcessor(Processor):
     """Reads Departments CSV file and enriches it."""
 
     def __init__(
         self,
-        s3_client: botocore.client.BaseClient | None = None,
+        s3_client: Any | None = None,
         s3_bucket: str = DEFAULT_S3_BUCKET,
     ):
         """Creates the DepartmentProcessor object."""
-        self.s3_client = s3_client
-        self.s3_bucket = s3_bucket
-        self.department_source: str | StringIO | None = None
-        self.processing_date: datetime.date | None = None
+        super().__init__(s3_client, s3_bucket)
         self._departments: geopandas.GeoDataFrame | None = None
-
-    def _set_s3_client(self):
-        if not self.s3_client:
-            self.s3_client = boto3.client("s3")
 
     @property
     def departments(self):
@@ -38,7 +31,7 @@ class DepartmentProcessor:
         return self._departments
 
     def _csv_to_geodataframe(self):
-        df = pandas.read_csv(self.department_source, sep=";", header=0)
+        df = pandas.read_csv(self.source, sep=";", header=0)
 
         if df.shape[0] == 0:
             raise ValueError("Cannot process empty file")
@@ -52,25 +45,9 @@ class DepartmentProcessor:
         """Adds the centroid (or barycentre) to the dataframe."""
         self.departments["centroid"] = self.departments["geometry"].centroid
 
-    def store(self):
-        """Persists the departments object."""
-        s3_target = f"departments/{self.processing_date.strftime('%Y%m%d')}.parquet"
-        buffer = BytesIO()
-        self.departments.to_parquet(buffer, compression="gzip")
-        self.s3_client.put_object(
-            Bucket=self.s3_bucket, Key=s3_target, Body=buffer.getvalue()
-        )
-
-        return self.s3_bucket, s3_target
-
-    def init(self, department_source: str | StringIO, processing_date: datetime.date):
-        """Initialize the main fields."""
-        self.department_source = department_source
-        self.processing_date = processing_date
-
-    def run(self, department_source: str | StringIO, processing_date: datetime.date):
-        """Orchestrates the department processing."""
-        self.init(department_source, processing_date)
-        self._set_s3_client()
+    def run(self, source: str | StringIO, processing_date: datetime.date):
+        """Orchestrates processor's tasks."""
+        self.init(source, processing_date)
         self.add_centroid()
-        return self.store()
+        self._set_s3_client()
+        return self.store("departments", self.departments)
