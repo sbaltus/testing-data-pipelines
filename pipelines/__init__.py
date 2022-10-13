63a87cbf5b828ac39e6893a87a9e7c27d1b92ea1
Stéphanie <10993200+sbaltus@users.noreply.github.com>
2022-10-13 12:06:14 +0200

    feat: get and store pollution data (#9)

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 DEFAULT_S3_BUCKET = "a-random-bucket"
 DEFAULT_AWS_REGION = "eu-west-3"
 
@@ -9,3 +11,6 @@ DPT_DATA_SOURCES = {
     "demographyref-france-pop-legale-departement-millesime/download/?format=csv"
     "&timezone=Europe/Berlin&lang=en&use_labels_for_header=true&csv_separator=%3B",
 }
+
+
+OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
--- pipelines/tasks/http.py
+++ pipelines/tasks/http.py
@@ -1,8 +1,17 @@
+import datetime
+
 import boto3
 import requests
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from botocore.client import BaseClient
 
-from pipelines import DEFAULT_S3_BUCKET
+from pipelines import DEFAULT_S3_BUCKET, OPENWEATHER_API_KEY
+from pipelines.tasks.pollution.models import (
+    AirQuality,
+    AirQualityFromOpenWeather,
+    PollutionForPoint,
+    PollutionFromOpenWeather,
+)
 
 
 def download_and_store_to_s3(
@@ -10,6 +19,7 @@ def download_and_store_to_s3(
     filename: str,
     s3: BaseClient | None = None,
     bucket=DEFAULT_S3_BUCKET,
+    s3_hook: S3Hook | None = None,
 ) -> str:
     """Download URL content and store it on s3."""
     response = requests.get(url, timeout=2)
@@ -22,3 +32,33 @@ def download_and_store_to_s3(
         s3 = boto3.client("s3")
     s3.put_object(Bucket=bucket, Key=filename, Body=response.content)
     return f"s3://{bucket}/{filename}"
+
+
+def get_pollution_data_for_point(
+    lat: float,
+    long: float,
+    start_date: datetime.datetime,
+    department_id: int,
+) -> PollutionForPoint:
+    start = int(start_date.timestamp())
+    url = f"https://api.openweathermap.org/data/2.5/air_pollution/history?lat={lat}&lon={long}&start={start}" \
+          f"&end={start}&appid={OPENWEATHER_API_KEY}"
+    response = requests.get(url, timeout=2)
+    response.raise_for_status()
+    content = response.json()
+    if len(content["list"]) != 1:
+        raise ValueError("unprocessable payload")
+
+    poll = PollutionFromOpenWeather(
+        coord=content["coord"], list=[AirQualityFromOpenWeather(**content["list"][0])]
+    )
+
+    return PollutionForPoint(
+        department_id=department_id,
+        centroid=[lat, long],
+        values=AirQuality(
+            timestamp=poll.list[0].dt,
+            main=poll.list[0].main,
+            components=poll.list[0].components,
+        ),
+    )
